564747f17cc8096b643326c8e0061062b6a3e7ea
Tom Gilbert <tom@alteredworlds.com>
2014-07-30 11:54:51 +0100

    About page looking pretty good. Sign Out button does nothing & version hardcoded in string resource, but apart from that not bad

--- app/src/main/java/com/alteredworlds/buddyfied/AboutFragment.java
+++ app/src/main/java/com/alteredworlds/buddyfied/AboutFragment.java
@@ -1,10 +1,12 @@
 package com.alteredworlds.buddyfied;
 
 import android.os.Bundle;
+import android.text.method.LinkMovementMethod;
 import android.view.LayoutInflater;
 import android.support.v4.app.Fragment;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 /**
  * Created by twcgilbert on 30/07/2014.
@@ -18,6 +20,8 @@ public class AboutFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_about, container, false);
+        TextView buddyfiedHelpLink = (TextView)rootView.findViewById(R.id.about_help_link_textview);
+        buddyfiedHelpLink.setMovementMethod(LinkMovementMethod.getInstance());
         return rootView;
     }
 }

5ad402f781085b4ab66e3c7ce825a6e11172aee7
Tom Gilbert <twcgilbert@gmail.com>
2014-08-22 10:14:30 +0100

    Put Buddies returned from Search into database (NOTE: need to get rid of these results any time search changes, plus on user explicit request to search again)

--- app/src/main/java/com/alteredworlds/buddyfied/service/BuddyQueryService.java
+++ app/src/main/java/com/alteredworlds/buddyfied/service/BuddyQueryService.java
@@ -1,6 +1,7 @@
 package com.alteredworlds.buddyfied.service;
 
 import android.app.IntentService;
+import android.content.ContentValues;
 import android.content.Intent;
 import android.database.Cursor;
 import android.net.Uri;
@@ -8,8 +9,10 @@ import android.os.Bundle;
 import android.os.ResultReceiver;
 import android.util.Log;
 
+import com.alteredworlds.buddyfied.R;
 import com.alteredworlds.buddyfied.Settings;
 import com.alteredworlds.buddyfied.data.BuddyfiedContract;
+import com.alteredworlds.buddyfied.data.BuddyfiedContract.BuddyEntry;
 import com.alteredworlds.buddyfied.data.BuddyfiedContract.ProfileAttributeListEntry;
 import com.alteredworlds.buddyfied.data.BuddyfiedProvider;
 
@@ -56,9 +59,8 @@ public class BuddyQueryService extends IntentService {
         } else {
             String errorMessage = "Unknown xmlrpc method call: '" + method + "'";
             Log.e(LOG_TAG, errorMessage);
-            Bundle res = new Bundle();
-            res.putString(RESULT_DESCRIPTION, errorMessage);
-            result = new CallResult(-1, res);
+            Bundle resultBundle = getResultBundleWithDescription(errorMessage);
+            result = new CallResult(-1, resultBundle);
         }
         if (null != resultReceiver) {
             resultReceiver.send(result.code, result.results);
@@ -66,21 +68,20 @@ public class BuddyQueryService extends IntentService {
     }
 
     private CallResult verifyConnection(Intent intent) {
-        CallResult retVal = null;
+        int resultCode = 0;
+        Bundle resultBundle = null;
         String username = Settings.getUsername(this);
         String password = Settings.getPassword(this);
         String uri = Settings.getBuddySite(this) + BuddyXmlRpcRoot;
         XMLRPCClient client = new XMLRPCClient(uri);
         try {
             client.call(VerifyConnection, username, password);
-            retVal = new CallResult(0, null);
         } catch (XMLRPCException e) {
             e.printStackTrace();
-            Bundle res = new Bundle();
-            res.putString(RESULT_DESCRIPTION, e.getLocalizedMessage());
-            retVal = new CallResult(-1, res);
+            resultBundle = getResultBundleWithDescription(e.getLocalizedMessage());
+            resultCode = -1;
         }
-        return retVal;
+        return new CallResult(resultCode, resultBundle);
     }
 
     private CallResult getMemberInfo(Intent intent) {
@@ -135,30 +136,86 @@ public class BuddyQueryService extends IntentService {
     }
 
     private CallResult getMatches(Intent intent) {
-        CallResult retVal = null;
+        int resultCode = 0;
+        Bundle resultBundle = null;
         //
-        // first we need the query data
         HashMap<String, Object> data = getSearchParameters(intent);
-        if (!data.isEmpty()) {
+        if (data.isEmpty()) {
+            resultBundle = getResultBundleWithDescription(getString(R.string.specify_query));
+        } else {
             String uri = Settings.getBuddySite(this) + BuddyXmlRpcRoot;
             XMLRPCClient client = new XMLRPCClient(uri);
             try {
-                // OK, go ahead and call...
                 Object res = client.call(
                         GetMatches,
                         Settings.getUsername(this),
                         Settings.getPassword(this),
                         data);
-                retVal = new CallResult(0, null);
+                String resultMessage = processSearchResults(res);
+                if ((null != resultMessage) && (resultMessage.length() > 0)) {
+                    resultBundle = getResultBundleWithDescription(resultMessage);
+                }
             } catch (XMLRPCException e) {
                 e.printStackTrace();
-                Bundle res = new Bundle();
-                res.putString(RESULT_DESCRIPTION, e.getLocalizedMessage());
-                retVal = new CallResult(-1, res);
+                resultBundle = getResultBundleWithDescription(e.getLocalizedMessage());
+                resultCode = -1;
+            }
+        }
+        return new CallResult(resultCode, resultBundle);
+    }
+
+    private Bundle getResultBundleWithDescription(String description) {
+        Bundle retVal = new Bundle();
+        retVal.putString(RESULT_DESCRIPTION, description);
+        return retVal;
+    }
+
+    private String processSearchResults(Object res) {
+        String retVal = null;
+        if (res instanceof HashMap) {
+            Object message = ((HashMap) res).get("message");
+            if (message instanceof String) {
+                retVal = (String) message;
+            } else if (message instanceof Object[]) {
+                // these should be Buddies...
+                Object[] values = (Object[]) message;
+                ContentValues[] cva = new ContentValues[values.length];
+                for (int i = 0; i < values.length; i++) {
+                    cva[i] = createBuddyValues((HashMap) values[i]);
+                }
+                int numBuddiesInserted = getContentResolver().bulkInsert(BuddyEntry.CONTENT_URI, cva);
+                Log.i(LOG_TAG, "Inserted " + numBuddiesInserted + " buddies");
             }
         }
         return retVal;
     }
+
+    private static ContentValues createBuddyValues(HashMap buddy) {
+        ContentValues retVal = new ContentValues();
+        retVal.put(BuddyEntry.COLUMN_NAME, (String) buddy.get("field_1"));
+        retVal.put(BuddyEntry.COLUMN_COMMENTS, (String) buddy.get("field_167"));
+        retVal.put(BuddyEntry.COLUMN_IMAGE_URI, getBuddyImageUrl(buddy));
+        retVal.put(BuddyEntry.COLUMN_AGE, (String) buddy.get("field_8"));
+        retVal.put(BuddyEntry.COLUMN_COUNTRY, (String) buddy.get("field_6"));
+        retVal.put(BuddyEntry.COLUMN_GAMEPLAY, (String) buddy.get("field_5"));
+        retVal.put(BuddyEntry.COLUMN_LANGUAGE, (String) buddy.get("field_153"));
+        retVal.put(BuddyEntry.COLUMN_PLATFORM, (String) buddy.get("field_2"));
+        retVal.put(BuddyEntry.COLUMN_PLAYING, (String) buddy.get("field_4"));
+        retVal.put(BuddyEntry.COLUMN_SKILL, (String) buddy.get("field_7"));
+        retVal.put(BuddyEntry.COLUMN_TIME, (String) buddy.get("field_152"));
+        retVal.put(BuddyEntry.COLUMN_VOICE, (String) buddy.get("field_9"));
+        return retVal;
+    }
+
+    private static String getBuddyImageUrl(HashMap buddy) {
+        String retVal = "";
+        Object sub = buddy.get("avatar");
+        if (sub instanceof HashMap) {
+            retVal = (String) ((HashMap) sub).get("full");
+        }
+        return retVal;
+    }
+
 
     private static class CallResult {
         public int code;

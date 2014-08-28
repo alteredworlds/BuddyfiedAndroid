37cceb6f8af059f05cb7da0d3d8105f7a4da33e5
Tom Gilbert <twcgilbert@gmail.com>
2014-08-28 12:13:31 +0100

    First pass at displaying search results including async image downloads which are SO SLOW!

--- app/src/main/java/com/alteredworlds/buddyfied/MatchedFragment.java
+++ app/src/main/java/com/alteredworlds/buddyfied/MatchedFragment.java
@@ -1,35 +1,106 @@
 package com.alteredworlds.buddyfied;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.CursorLoader;
+import android.support.v4.content.Loader;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.GridView;
 
+import com.alteredworlds.buddyfied.data.BuddyfiedContract.BuddyEntry;
 import com.alteredworlds.buddyfied.service.BuddyQueryService;
+import com.alteredworlds.buddyfied.view_model.MatchedAdapter;
 
 /**
  * Created by twcgilbert on 30/07/2014.
  */
-public class MatchedFragment extends Fragment {
+public class MatchedFragment extends Fragment implements LoaderManager.LoaderCallbacks<Cursor> {
+    private static final String LOG_TAG = MatchedFragment.class.getSimpleName();
+
+    private static final int MATCHED_LOADER = 0;
+
+    public static final String[] BuddyColumns = {
+            BuddyEntry._ID,
+            BuddyEntry.COLUMN_NAME,
+            BuddyEntry.COLUMN_IMAGE_URI
+    };
+
+    public static final int COL_NAME = 1;
+    public static final int COL_IMAGE_URI = 2;
+
+    private Uri mQuery;
+    private MatchedAdapter mMatchedAdaptor;
 
     public MatchedFragment() {
+        mQuery = BuddyEntry.CONTENT_URI;
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        getLoaderManager().initLoader(MATCHED_LOADER, null, this);
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_matched, container, false);
+
+        mMatchedAdaptor = new MatchedAdapter(getActivity(), null, 0);
+
+        final GridView gridView = (GridView) rootView.findViewById(R.id.matched_gridview);
+        gridView.setAdapter(mMatchedAdaptor);
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                ;
+            }
+        });
+
         return rootView;
     }
 
     @Override
     public void onResume() {
         super.onResume();
+        // this code is ALWAYS QUERYING
         Intent intent = new Intent(getActivity(), BuddyQueryService.class);
         intent.putExtra(BuddyQueryService.METHOD_EXTRA, BuddyQueryService.GetMatches);
         intent.putExtra(BuddyQueryService.PROFILE_ID_EXTRA, ((MainActivity) getActivity()).getSearchProfileId());
         getActivity().startService(intent);
+        // starts the loader to view existing data
+        //getLoaderManager().restartLoader(MATCHED_LOADER, null, this);
+    }
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+        return new CursorLoader(
+                getActivity(),
+                mQuery,
+                MatchedFragment.BuddyColumns,
+                null,
+                null,
+                null
+        );
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+        Log.d(LOG_TAG, "onLoadFinished");
+        mMatchedAdaptor.swapCursor(data);
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+        Log.d(LOG_TAG, "onLoaderReset");
+        mMatchedAdaptor.swapCursor(null);
     }
 }
---
+++ app/src/main/java/com/alteredworlds/buddyfied/view_model/MatchedAdapter.java
@@ -0,0 +1,56 @@
+package com.alteredworlds.buddyfied.view_model;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.support.v4.widget.CursorAdapter;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.alteredworlds.buddyfied.ImageDownloader;
+import com.alteredworlds.buddyfied.MatchedFragment;
+import com.alteredworlds.buddyfied.R;
+
+/**
+ * Created by twcgilbert on 22/08/2014.
+ */
+public class MatchedAdapter extends CursorAdapter {
+    private final ImageDownloader imageDownloader = new ImageDownloader();
+
+    public MatchedAdapter(Context context, Cursor c, int flags) {
+        super(context, c, flags);
+    }
+
+    @Override
+    public View newView(Context context, Cursor cursor, ViewGroup parent) {
+        View view = LayoutInflater.from(context).inflate(R.layout.list_item_matched, parent, false);
+        ViewHolder holder = new ViewHolder(view);
+        view.setTag(holder);
+        return view;
+    }
+
+
+    @Override
+    public void bindView(View view, Context context, Cursor cursor) {
+        ViewHolder viewHolder = (ViewHolder) view.getTag();
+        String name = cursor.getString(MatchedFragment.COL_NAME);
+        viewHolder.textView.setText(name);
+        String imageUrl = cursor.getString(MatchedFragment.COL_IMAGE_URI);
+        imageDownloader.download(imageUrl, viewHolder.imageView);
+    }
+
+    /**
+     * Cache of the child views for an attribute list item.
+     */
+    public static class ViewHolder {
+        public final TextView textView;
+        public final ImageView imageView;
+
+        public ViewHolder(View view) {
+            textView = (TextView) view.findViewById(R.id.list_item_matched_text);
+            imageView = (ImageView) view.findViewById(R.id.list_item_matched_image);
+        }
+    }
+}

b1175f1bf9e8c03bdc3685011424c912377461ff
Tom Gilbert <tom@alteredworlds.com>
2014-09-23 12:04:36 +0100

    should have gone in with last commit

---
+++ app/src/main/java/com/alteredworlds/buddyfied/view_model/ListItemId.java
@@ -0,0 +1,11 @@
+package com.alteredworlds.buddyfied.view_model;
+
+/**
+ * Created by twcgilbert on 22/09/2014.
+ */
+public final class ListItemID {
+    public static final int SEARCH_VIEW_TYPE_ID = 0;
+    public static final int COMMENTS_VIEW_TYPE_ID = 1;
+    public static final int PROFILE_VIEW_TYPE_ID = 2;
+    public static final int BUDDY_HEADER_VIEW_TYPE_ID = 3;
+}

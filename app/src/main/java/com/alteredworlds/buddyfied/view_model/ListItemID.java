0ce2f5be9f07a8c1fe76569f89bf061690edb726
Tom Gilbert <tom@alteredworlds.com>
2014-10-07 13:36:21 +0100

    trying to sort out file caps issue

--- app/src/main/java/com/alteredworlds/buddyfied/view_model/ListItemID.java
+++ app/src/main/java/com/alteredworlds/buddyfied/view_model/ListItemID.java
@@ -1,8 +1,13 @@
+/*
+ * Copyright (c) 2014 Tom Gilbert <tom@alteredworlds.com> - All rights reserved.
+ *
+ * This file is part of Buddyfied Android.
+ *
+ * For applicable license please see LICENSE included with this distribution.
+ */
+
 package com.alteredworlds.buddyfied.view_model;
 
-/**
- * Created by twcgilbert on 22/09/2014.
- */
 public final class ListItemID {
     public static final int SEARCH_VIEW_TYPE_ID = 0;
     public static final int COMMENTS_VIEW_TYPE_ID = 1;
